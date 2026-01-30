f6c11a2605887360d9e9e4ae4af79a0886c841b3
Sean Lai <seanlai221@gmail.com>
2026-01-30 13:58:30 -0500

    Fixed large layout for the about section

--- components/sections/about.tsx
+++ components/sections/about.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import TwoColumnSection from '../ui/TwoColumnSection'
 import { aboutSectionContent } from '@/lib/sectionContent'
+import { Square } from 'lucide-react'
 
 const About = () => {
   return (
-    <section className='w-[100svw] lg:mx-auto lg:container px-10 min-h-[40vh] py-20'>
-      <h1 className='pb-8'>About Us</h1>
-      <div className='min-h-[50vh]'>
+    <section className='w-[100svw] py-20'>
+      <h1 className='pb-8 lg:container w-[100svw] flex flex-1 gap-1 px-10 lg:px-[3.4rem] xl:px-[6.6rem]'>
+        <Square className='h-6 w-6' />
+        About Us
+      </h1>
+      <div className='min-h-[50vh]lg:container w-full px-10 lg:px-[3.4rem] xl:px-[6.6rem] '>
         <TwoColumnSection {...aboutSectionContent[0]} />
         <TwoColumnSection {...aboutSectionContent[1]} />
         <TwoColumnSection {...aboutSectionContent[2]} />
--- components/ui/TwoColumnSection.tsx
+++ components/ui/TwoColumnSection.tsx
@@ -10,23 +10,21 @@ interface TwoColumnSectionProps {
 
 const TwoColumnSection = ({ title, description, image, flip }: TwoColumnSectionProps) => {
   return (
-    <div className='lg:grid lg:grid-cols-2'>
-      <div className={`py-5 lg:p-[8rem] ${flip ? 'lg:order-2' : ''}`}>
-        <h2 className='text-5xl font-bold'>{title}</h2>
+    <div className='lg:grid lg:grid-cols-2 outline-1 outline-white/60'>
+      {/* <span className='border-t border-4 border-red-500/60' /> */}
+      {/* <span className='border-l-4 border-4 border-red-500/60' /> */}
+      <div className={`py-5 lg:p-[4rem] xl:p-[6rem] ${flip ? 'lg:order-2' : ''}`}>
+        <h2 className='md:text-4xl text-5xl font-bold'>{title}</h2>
         <br/>
         <br/>
-        <div>{description}</div>
+        <div className='text-lg'>{description}</div>
       </div>
-      <div className={`w-full h-full lg:px-[8rem] bg-amber-200 relative overflow-hidden ${flip ? 'lg:order-1' : ''}`}>
+      <div className='w-full h-full bg-red-50 items-center flex justify-center'>
+      <div className={`w-full aspect-square lg:py-0 lg:px-[6rem] bg-amber-200 relative overflow-hidden ${flip ? 'lg:order-1' : ''}`}>
         {image && (
-          <Image
-            src={image}
-            alt={title}
-            fill
-            className='object-cover aspect-square'
-            unoptimized
-          />
+          <img src={image} className="w-full h-auto object-contain" />
         )}
+      </div>
       </div>
     </div>
   )
